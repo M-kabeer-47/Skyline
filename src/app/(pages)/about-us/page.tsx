c1db06a484318c4851a5939f9f83a0302a99c99c
Muhammad Kabeer <muhammadkabeer2003@gmail.com>
2025-02-07 01:23:54 +0500

    page

--- src/app/(pages)/about-us/page.tsx
+++ src/app/(pages)/about-us/page.tsx
@@ -1,35 +1,6 @@
 import TurnaroundTime from "@/components/Home/TurnaroundTime"
 import { ArrowRight } from "lucide-react"
-// import { NumberMask } from "@/components/Home/NumberMask"
-export function NumberMask() {
-    return (
-      <div className="relative flex items-center justify-center w-full">
-        <div className="flex flex-col">
-
-        
-        <div
-          className="flex lg:text-[500px] sm:text-[340px] text-[250px] font-bold leading-none text-transparent bg-clip-text relative top-[-90px]"
-          style={{
-            backgroundImage: `url(${"/backgrounds/bg-cover.jpg"})`,
-            backgroundSize: "100% 100%",
-            backgroundPosition: "center",
-            WebkitTextFillColor: "transparent",
-            WebkitBackgroundClip: "text",
-            backgroundClip: "text",
-          }}
-        >
-          30
-        </div>
-        <p className="relative top-[-100px] text-xl font-semibold text-[#6ec1e4] leading-[30px] md:max-w-[600px] text-center">
-        Years Of Experience With <span className="font-bold text-2xl">Creative Team</span>
-        </p>
-        </div>
-        
-        
-
-      </div>
-    )
-  }
+import NumberMask from "@/components/layout/NumberMask"
   
   
 
