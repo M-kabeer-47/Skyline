afd8602ff0b1eb263512ba1cb20faed13fd23cc1
Muhammad Kabeer <muhammadkabeer2003@gmail.com>
2025-02-07 01:12:57 +0500

    fort

--- src/components/Home/Stats.tsx
+++ src/components/Home/Stats.tsx
@@ -44,7 +44,7 @@ export default function StatsSection() {
       >
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 gap-y-16 text-white text-center">
-            {stats.map((stat, index) => (
+            {stats.map((stat, index) => ( // @ts-ignore
               <StatItem key={index} containerRef={containerRef} {...stat} />
             ))}
           </div>
